53b4d91ba5e9ba33c61d5721303db17b9fb6b32f
ReLoia <zrennyz@gmail.com>
2025-01-14 00:22:56 +0100

    fix: General bug fixes

--- src/main/java/it/reloia/tecnogui/mixin/BossBarHudMixin.java
+++ src/main/java/it/reloia/tecnogui/mixin/BossBarHudMixin.java
@@ -1,20 +1,14 @@
 package it.reloia.tecnogui.mixin;
 
-import com.google.common.collect.Maps;
 import it.reloia.tecnogui.client.TecnoGUIClient;
 import net.minecraft.client.gui.DrawContext;
 import net.minecraft.client.gui.hud.BossBarHud;
-import net.minecraft.client.gui.hud.ClientBossBar;
+import net.minecraft.entity.boss.BossBar;
 import net.minecraft.text.Text;
-import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-
-import java.util.Map;
-import java.util.UUID;
 
 /**
  * Features of this mixin:<br><br>
@@ -23,17 +17,14 @@ import java.util.UUID;
  */
 @Mixin(BossBarHud.class)
 public abstract class BossBarHudMixin {
-    @Final
-    @Shadow
-    final Map<UUID, ClientBossBar> bossBars = Maps.newLinkedHashMap();
-
-    @Inject(method = "render", at = @At("HEAD"))
-    private void tecnogui$getBossBarNameOnRender(DrawContext context, CallbackInfo ci) {
-        for (ClientBossBar clientBossBar : this.bossBars.values()) {
-            String content = clientBossBar.getName().getString();
-            if (content.equals("\uD875\uDEC2") && TecnoGUIClient.CONFIG.isHideVoteAds()) {
-                clientBossBar.setName(Text.of(""));
+    @Inject(method = "renderBossBar*", at = @At("HEAD"), cancellable = true)
+    private void tecnogui$hideVoteAds(DrawContext context, int x, int y, BossBar bossBar, CallbackInfo ci) {
+        String content = bossBar.getName().getString();
+        if (TecnoGUIClient.CONFIG.isHideVoteAds()) {
+            if (content.equals("\uD875\uDEC2")) {
+                bossBar.setName(Text.of(""));
             }
+            ci.cancel();
         }
     }
 }

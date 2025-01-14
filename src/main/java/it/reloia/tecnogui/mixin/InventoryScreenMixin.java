a3d8b55d87d8a71351a00e02ed7ecd2bfaeeba34
ReLoia <zrennyz@gmail.com>
2025-01-14 14:51:58 +0100

    fix: Small code changes to CustomButtons

--- src/main/java/it/reloia/tecnogui/mixin/InventoryScreenMixin.java
+++ src/main/java/it/reloia/tecnogui/mixin/InventoryScreenMixin.java
@@ -35,15 +35,17 @@ public abstract class InventoryScreenMixin extends AbstractInventoryScreen<Playe
 
     @Inject(method = "init", at = @At("TAIL"))
     private void onInit(CallbackInfo ci) {
+        // todo: make customButtons configurable
         this.customButton = new ImageButton(
                 this.x - 20,
                 this.y + 2,
                 20, 20,
-                new Identifier("tecnogui", "textures/gui/garage.png"),
+                new Identifier("tecnogui",  "textures/gui/garage.png"),
                 (button) -> {
                     if (MinecraftClient.getInstance().getNetworkHandler() != null)
                         MinecraftClient.getInstance().getNetworkHandler().sendCommand("garage");
-                }
+                },
+                "Garage"
         );
 
         this.addDrawableChild(customButton);
